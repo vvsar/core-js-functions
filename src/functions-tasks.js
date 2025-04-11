60b209ce8a2a26e0ac2f22c38a020a814dc94b1f
Victor Saraev <vvsar@mail.ru>
2025-04-11 03:44:21 +0300

    feat: add getIdGeneratorFunction solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -207,8 +207,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom - 1;
+  function f() {
+    id += 1;
+    return id;
+  }
+  return f;
 }
 
 module.exports = {
